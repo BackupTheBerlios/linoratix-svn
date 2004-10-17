5c858a6e873dde233087d7d37dcc6d06d97927b0
pr0gm4 <pr0gm4@3d6933e5-cfd4-0310-958f-ae10e039d79c>
2004-10-17 18:46:15 +0000

    bugfixes  * ein richtiges perl module draus gemacht ...
    update
     * motd und issue

--- dev/linoratix-config/modules/base.pm
+++ dev/linoratix-config/modules/base.pm
@@ -9,11 +9,9 @@ use strict;
 
 use vars qw(@ISA @EXPORT $MOD_VERSION);
 use Exporter;
-$MOD_VERSION = "0.0.1";
+$MOD_VERSION = "0.0.2";
 @ISA = qw(Exporter);
 @EXPORT = qw(&new);
-
-our $linoratix = Linoratix->new();
 
 # namespace [100-199]
 
@@ -25,8 +23,10 @@ sub new
 
 	bless($self, $class);
 
-	help() if($linoratix->option("help"));
-	beep_on_off() if($linoratix->param("beep"));
+	$self->help() if($self->option("help"));
+	$self->beep_on_off() if($self->param("beep"));
+	$self->set_motd() if($self->param("motd"));
+	$self->set_issue() if($self->param("issue"));
 	
 	return $self;
 }
@@ -35,10 +35,40 @@ sub help
 {
 	my $self = shift;
 
-	$linoratix->message("linoratix-config $BASE_VERSION\n");
-	$linoratix->message("	module: base $MOD_VERSION\n\n");
+	$self->message("linoratix-config $BASE_VERSION\n");
+	$self->message("	module: base $MOD_VERSION\n\n");
 	print "	--help 			display help message\n";
-	print "	--beep [on|off]		turns console beep on or off\n\n";
+	print "	--beep <on|off>		turns console beep on or off\n";
+	print "	--motd <filename>	set a new message of the day\n";
+	print "	--issue <filename>	set a issue file\n\n";
+}
+
+sub set_motd
+{
+	my $self = shift;
+	my $file = $self->param("motd");
+		
+	unless(-f "$file") {
+		$self->error("Sorry, but $file is not a valid file!\n");
+		return 155;
+	}
+	system("/bin/cp -f $file /etc/motd");
+	print "\n";
+	$self->message("New message of the day installed.\n");
+}
+
+sub set_issue
+{
+	my $self = shift;
+	my $file = $self->param("issue");
+		
+	unless(-f "$file") {
+		$self->error("Sorry, but $file is not a valid file!\n");
+		return 155;
+	}
+	system("/bin/cp -f $file /etc/issue");
+	print "\n";
+	$self->message("New issue file installed.\n");
 }
 
 #
@@ -54,22 +84,22 @@ sub beep_on_off
 	close(FH);
 
 	# beep is on and should be on do nothing
-	if(($linoratix->param("beep") eq "on" && m/set bell-style on/) || ($linoratix->param("beep") eq "none" && m/set bell-style none/)) {
+	if(($self->param("beep") eq "on" && m/set bell-style on/) || ($self->param("beep") eq "none" && m/set bell-style none/)) {
 	}
 	# beep is on and should be off 
-	elsif($linoratix->param("beep") eq "off" && m/set bell-style on/) {
+	elsif($self->param("beep") eq "off" && m/set bell-style on/) {
 		s/set bell-style on/set bell-style none/gmis;
 	}
 	# beep is none and should be on
-	elsif($linoratix->param("beep") eq "on" && m/set bell-style none/) {
+	elsif($self->param("beep") eq "on" && m/set bell-style none/) {
 		s/set bell-style none/set bell-style on/gmis;
 	}
 	# beep is not defined and should be on
-	elsif($linoratix->param("beep") eq "on") {
+	elsif($self->param("beep") eq "on") {
 		$_ .= "\nset bell-style on\n";
 	}
 	# beep is not defined and should be off
-	elsif($linoratix->param("beep") eq "off") {
+	elsif($self->param("beep") eq "off") {
 		$_ .= "\nset bell-style none\n";
 	} else {
 		exit 101;
