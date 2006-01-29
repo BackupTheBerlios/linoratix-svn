e20ba5eb9416b6fa783a6b645d4b5f35f2304c2f
pr0gm4 <pr0gm4@3d6933e5-cfd4-0310-958f-ae10e039d79c>
2006-01-29 18:14:05 +0000

    stage2

--- dev/distribution/lip/lbuild/stage2.sh
+++ dev/distribution/lip/lbuild/stage2.sh
@@ -4,10 +4,10 @@
 # script fuer stage 2
 #
 
-export BUILD_BUILD_ENV=1
+export BUILD_BUILD_ENV=0
 export STAGE=2
 
-mkdir -pv /tools/{proc,sys}
+mkdir -pv /tools/{proc,sys,dev}
 mount -vt proc proc /tools/proc
 mount -vt sysfs sysfs /tools/sys
 
@@ -19,8 +19,22 @@ cd /tools
 ln -s . tools
 cd $OLDPWD
 
+mkdir -p /tools/root
+
+echo "#!/bin/bash" > /tools/stage2.sh
+echo "set +h" >> /tools/stage2.sh
+echo "echo Welcome to STAGE 2" >> /tools/stage2.sh
+
+chmod 700 /tools/stage2.sh
+
 chroot "/tools" /tools/bin/env -i \
     HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
-    /tools/bin/bash --login +h
+    /tools/stage2.sh
 
+
+
+#umount /tools/proc
+#umount /tools/sys
+#umount /tools/dev/*
+#umount /tools/dev
